c44ebde0b0849197f9611e66bf68502a5a920f1a
BxinSeven12th <junbin712@sina.com>
2026-06-01 16:19:52 +0800

    fix: submit code

--- env.d.ts
+++ env.d.ts
@@ -5,3 +5,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.css?inline' {
+  const css: string
+  export default css
+}
